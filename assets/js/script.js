42a468731c538442317c0891e92c977740dce8ae
arslandev51 <161108212+arslandev51@users.noreply.github.com>
2024-10-10 08:17:23 +0500

    now active

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,15 @@ window.addEventListener("load", ()=>{
 });
 
 // const activeMenu = document.querySelector("[data-header]");
+
+// add event on multiple elements
+const addEventOnElements  function (elements, eventType, callback){
+    for(let i=0, len=elements.lenght; i<len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+// Navbar toggler for mobile
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
